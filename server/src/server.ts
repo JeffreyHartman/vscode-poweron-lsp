7af1f0a1177e86f50f3e3ac37fdbacf674e0b255
Jeff Hartman <dracul104@gmail.com>
2024-07-12 12:13:51 -0400

    Message buffer

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,46 @@
 import log from "./log";
+import { initialize } from "./methods/initialize";
+
+interface Message {
+  jsonrpc: string;
+}
+
+export interface RequestMessage extends Message {
+  id: number | string;
+  method: string;
+  params?: unknown[] | object;
+}
+
+let buffer = ""; // Buffer to accumulate chunks of data as they are received
 
 process.stdin.on("data", (chunk) => {
-  log.write(chunk.toString());
+  buffer += chunk;
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    // Check for the content-length header
+    const lengthMatch = buffer.match(/Content-Length: (\d+)\r\n/);
+    if (!lengthMatch) {
+      break;
+    }
+
+    const contentLength = parseInt(lengthMatch[1], 10);
+    // Start of the message is after a double newline
+    const messageStart = buffer.indexOf("\r\n\r\n") + 4;
+
+    // Continue unless the full message has been received
+    if (buffer.length < messageStart + contentLength) {
+      break;
+    }
+
+    const rawMessage = buffer.slice(messageStart, messageStart + contentLength);
+    const message = JSON.parse(rawMessage);
+
+    log.write({ id: message.id, method: message.method });
+
+    // TODO: call method and respond
+
+    // Remove the processed message from the buffer
+    buffer = buffer.slice(messageStart + contentLength);
+  }
 });
